aba7e6da9582b38163cd4621f8ebb0342a1a4a93
lamak <lamak.qaizar@careem.com>
2021-04-06 10:26:02 +0500

    empty test
    Co-authored-by: Talha Siddiqui <talhasiddiqui190@gmail.com>

--- src/test/telemetrysystem/TelemetryDiagnosticControlsTest.kt
+++ src/test/telemetrysystem/TelemetryDiagnosticControlsTest.kt
@@ -6,51 +6,7 @@ import org.junit.Test
 
 class TelemetryDiagnosticControlsTest {
 
-    private lateinit var telemetry: TelemetryDiagnosticControls
-    private lateinit var client: TelemetryClientDouble
-
-    @Before
-    fun setUp() {
-        client = TelemetryClientDouble()
-        telemetry = TelemetryDiagnosticControls(client)
-    }
-
     @Test
-    fun `should disconnect and connect after X retries`() {
-        telemetry.checkTransmission()
-        client.wasDisconnected()
-    }
-
-    @Test(expected = Exception::class)
-    fun `unable to connect after retries`() {
-        client.initWith(4)
-        telemetry.checkTransmission()
-    }
-}
-
-class TelemetryClientDouble() : TelemetryClient() {
-
-    private var wasDisconnected: Boolean = false
-    private var retries: Int = 0
-    private var connectAfterRetries: Int = 1
-
-    override fun disconnect() {
-        wasDisconnected = true
-    }
-
-    fun wasDisconnected() {
-        Assert.assertTrue(wasDisconnected)
-    }
-
-    override fun connect(telemetryServerConnectionString: String?) {
-        retries++
-
-        if (retries == connectAfterRetries) {
-            onlineStatus = true
-        }
-    }
-
-    fun initWith(connectAfterRetries: Int) {
-        this.connectAfterRetries = connectAfterRetries
+    fun `should do something`() {
     }
 }
